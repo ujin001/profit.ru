90900043f8f5f14893fdf1886b8c3bfc5c644d1f
bil <bpa@albatrosscom.ru>
2019-03-23 21:38:25 +0700

    POST only

--- mail.php
+++ mail.php
@@ -1,4 +1,8 @@
 <?php
+
+    if (empty($_POST)) {
+        exit;
+    }
     function HTTPPost ($url, array $params) { $query = http_build_query($params); $ch = curl_init(); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); curl_setopt($ch, CURLOPT_HEADER, false); curl_setopt($ch, CURLOPT_URL, $url); curl_setopt($ch, CURLOPT_POST, true); curl_setopt($ch, CURLOPT_POSTFIELDS, $query); $response = curl_exec($ch); curl_close($ch); return $response; }
 
     $email  = htmlspecialchars($_POST['email']);
