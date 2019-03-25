6c447e25d9dbbf80aa68bb25051b0eea85be347e
bil <bil@novotelecom.ru>
2019-03-25 17:01:20 +0700

    favicon

--- mail.php
+++ mail.php
@@ -1,9 +1,10 @@
 <?php
 
-    if (empty($_POST)) {
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         exit;
     }
-    function HTTPPost ($url, array $params) { $query = http_build_query($params); $ch = curl_init(); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); curl_setopt($ch, CURLOPT_HEADER, false); curl_setopt($ch, CURLOPT_URL, $url); curl_setopt($ch, CURLOPT_POST, true); curl_setopt($ch, CURLOPT_POSTFIELDS, $query); $response = curl_exec($ch); curl_close($ch); return $response; }
+
+function HTTPPost ($url, array $params) { $query = http_build_query($params); $ch = curl_init(); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); curl_setopt($ch, CURLOPT_HEADER, false); curl_setopt($ch, CURLOPT_URL, $url); curl_setopt($ch, CURLOPT_POST, true); curl_setopt($ch, CURLOPT_POSTFIELDS, $query); $response = curl_exec($ch); curl_close($ch); return $response; }
 
     $email  = htmlspecialchars($_POST['email']);
     $name  = htmlspecialchars($_POST['name']);
